3abed9f6b91bd9aabb2eef033c449f887f88f909
Denys-526 <d.o.chernetskyi@student.khai.edu>
2026-02-25 04:18:13 +0200

    practical 1

---
+++ main.py
@@ -0,0 +1,19 @@
+﻿import speech_recognition as srec
+
+
+def recognize_speech(rec, mic):
+    with mic as source:
+        rec.adjust_for_ambient_noise(source)
+        audio = rec.listen(source)
+
+    result = {"Текст": None}
+    result["Текст"] = rec.recognize_google(audio, show_all=False, language="uk-UA")
+    return result
+
+
+if __name__ == "__main__":
+    recognizer = srec.Recognizer()
+    mic = srec.Microphone(device_index=1)
+    while True:
+        result = recognize_speech(recognizer, mic)
+        print("Ви сказали: \n{}".format(result["Текст"]))
